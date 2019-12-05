e82d983323de2ad6ea65a165df5f393700753902
Myles Dewaal <52334937+myles-dewaal@users.noreply.github.com>
2019-12-04 20:37:52 -0700

    api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,40 +1,31 @@
+/* eslint-disable prettier/prettier */
 var db = require("../models");
 var axios = require("axios");
 require("dotenv").config();
 
 module.exports = function (app) {
-  // Get all examples
-  app.get("/api/examples", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-
-  // Create a new example
-  app.post("/api/examples", function (req, res) {
-    db.Example.create(req.body).then(function (dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function (req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
-      res.json(dbExample);
-    });
-  });
 
   app.get("/api/movies", function (req, res) {
-    var queryURL = "https://api.themoviedb.org/3/movie/now_playing?api_key=" + process.env.APIKEY + "&language=en-US&page=1"
-    console.log('query string',queryURL)
+    var queryURL = "https://api.themoviedb.org/3/movie/now_playing?api_key=" + process.env.APIKEY + "&language=en-US&page=1";
     axios.get(queryURL
     ).then(function (response) {
-      console.log(response.data)
-      res.send(response.data)
+      console.log(response.data);
+      res.send(response.data);
     }).catch(function (error) {
       console.log(error);
-    })
-   
-  })
+    });
+
+  });
+
+  app.get("/api/movies/upcoming", function (req, res) {
+    var queryURL = "https://api.themoviedb.org/3/movie/upcoming?api_key=" + process.env.APIKEY + "&language=en-US&page=1";
+    axios.get(queryURL
+    ).then(function (response) {
+      console.log(response.data);
+      res.send(response.data);
+    }).catch(function (error) {
+      console.log(error);
+    });
+  });
 
 };
